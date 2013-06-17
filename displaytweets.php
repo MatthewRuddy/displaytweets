e1d0f4e3d239ae2bb9cc751e6212e08cbeec36bc
Jonwicks25 <Jonwicks25@gmail.com>
2013-06-17 23:34:54 +0200

    Update displaytweets.php
    URL for hashtags corrected

--- displaytweets.php
+++ displaytweets.php
@@ -498,7 +498,7 @@ class DisplayTweets {
         $text = preg_replace( "#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t< ]*)#", "\\1<a href=\"\\2\" target=\"_blank\">\\2</a>", $text );
         $text = preg_replace( "#(^|[\n ])((www|ftp)\.[^ \"\t\n\r< ]*)#", "\\1<a href=\"http://\\2\" target=\"_blank\">\\2</a>", $text );
         $text = preg_replace( "/@(\w+)/", "<a href=\"http://www.twitter.com/\\1\" target=\"_blank\">@\\1</a>", $text );
-        $text = preg_replace( "/#(\w+)/", "<a href=\"http://search.twitter.com/search?q=\\1\" target=\"_blank\">#\\1</a>", $text );
+        $text = preg_replace( "/#(\w+)/", "<a href=\"http://twitter.com/search?q=%23\\1&src=hash\" target=\"_blank\">#\\1</a>", $text );
         return $text;
     }
 
